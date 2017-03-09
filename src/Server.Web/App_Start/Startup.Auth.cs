1923dacdd08400480f9b7fdaaa50a6317e71522d
Mark Davies <mark.davies@esendex.com>
2017-03-09 15:04:40 +0000

    Removing uneeded stuff

--- src/Server.Web/App_Start/Startup.Auth.cs
+++ src/Server.Web/App_Start/Startup.Auth.cs
@@ -5,7 +5,6 @@ namespace Server.Web
 {
     public partial class Startup
     {
-
         public void ConfigureAuth(IAppBuilder app)
         {
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
