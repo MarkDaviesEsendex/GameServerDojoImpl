b67c3bdbf047dbe04129587b8ba056878310e596
Mark Davies <mark.davies@esendex.com>
2017-03-10 08:34:18 +0000

    Tests

---
+++ src/Server.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Server.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestThing()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
